752428d92b67058a92cad22e2c8fe3eb226158a0
TekniqueJoseph <tekniquejoseph@gmail.com>
2023-06-19 17:10:50 -0400

    added second class holding logic for list items

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,14 +1,37 @@
 class Product {
-    title = 'DEFAULT';
-    imageUrl;    
-    description;
-    price;
+    // title = 'DEFAULT';
+    // imageUrl;    
+    // description;
+    // price;
 
     constructor(title, image, desc, price){
         this.title = title;
         this.imageUrl = image;
         this.description = desc;
         this.price = price;
+    }
+}
+
+class ProductItem {
+    constructor(product) {
+        this.product = product
+    }
+
+    render() {
+         const prodEl = document.createElement('li');
+         prodEl.className = 'product-item';
+         prodEl.innerHTML = `
+                <div>
+                    <img src='${this.product.imageUrl}' alt='${this.product.description}'>
+                    <div class='product-item__content'>
+                        <h2>${this.product.title}</h2>
+                        <h3>\$${this.product.price}</h3>
+                        <p>${this.product.description}</p>
+                        <button>Add To Cart</button>
+                    </div>
+                </div>
+            `;
+        return prodEl;
     }
 }
 
@@ -33,19 +56,8 @@ class ProductList {
         const prodList = document.createElement('ul');
         prodList.className = 'product-list';
         for (const prod of this.products) {
-            const prodEl = document.createElement('li');
-            prodEl.className = 'product-item';
-            prodEl.innerHTML = `
-                <div>
-                    <img src='${prod.imageUrl}' alt='${prod.description}'>
-                    <div class='product-item__content'>
-                        <h2>${prod.title}</h2>
-                        <h3>\$${prod.price}</h3>
-                        <p>${prod.description}</p>
-                        <button>Add To Cart</button>
-                    </div>
-                </div>
-            `;
+           const productItem = new ProductItem(prod)
+           const prodEl = productItem.render()
             prodList.append(prodEl);
         }
         renderHook.append(prodList);
